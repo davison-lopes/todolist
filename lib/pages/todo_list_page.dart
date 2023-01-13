2a0719d7a432a277442a9614f8f698cfc42b1356
dlcode036462 <davisonluis4@gmail.com>
2023-01-13 15:40:32 -0300

    primeiro commit

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -3,43 +3,49 @@ import 'package:todo_list/model/todo.dart';
 import 'package:todo_list/widgets/todo_list_item.dart';
 
 class todo_list_page extends StatefulWidget {
-    todo_list_page({Key? key}) : super(key: key);
+  todo_list_page({Key? key}) : super(key: key);
 
   @override
   State<todo_list_page> createState() => _todo_list_pageState();
 }
 
 class _todo_list_pageState extends State<todo_list_page> {
-   final TextEditingController  todoController = TextEditingController();
+  final TextEditingController todoController = TextEditingController();
 
   List<Todo> todos = [];
+  Todo? todoDelete;
+  int? todoPosDelete;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  SafeArea(
+    return SafeArea(
       child: Scaffold(
         body: Center(
           child: Padding(
-            padding:  const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
                 Row(
-                  children:  [
+                  children: [
                     Expanded(
                       child: TextField(
-
-                          decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: "Adicionar Tarefa",
-                          hintText: "Ex.  Estudar Matematica"
-
-                        ),
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Adicionar Tarefa",
+                            hintText: "Ex.  Estudar Matematica"),
                         controller: todoController,
-
                       ),
                     ),
-                    const SizedBox(width: 8,),
+                    const SizedBox(
+                      width: 8,
+                    ),
                     ElevatedButton(
                       style: ElevatedButton.styleFrom(
                         primary: Colors.cyan,
@@ -51,73 +57,141 @@ class _todo_list_pageState extends State<todo_list_page> {
                       child: const Icon(
                         Icons.add,
                         size: 30,
-
                       ),
                       onPressed: () {
                         String title = todoController.text;
-                        setState(() {
-                          Todo newTodo = Todo(
-                              title: title,
-                              dateTime: DateTime.now()
-                          );
+                        setState(
+                          () {
+                            Todo newTodo =
+                                Todo(title: title, dateTime: DateTime.now());
 
-                          todos.add(newTodo);
-                        },
+                            todos.add(newTodo);
+                          },
                         );
                         todoController.clear();
                       },
                     ),
                   ],
                 ),
-                const SizedBox(height: 16,),
+                const SizedBox(
+                  height: 16,
+                ),
                 Flexible(
-
                   child: ListView(
                     shrinkWrap: true,
-
                     children: [
-                      for( Todo todo in todos )
+                      for (Todo todo in todos)
                         TodoListItem(
                           todo: todo,
+                          onDelete: onDelete,
                         ),
                     ],
-
                   ),
                 ),
-                const SizedBox(height: 16,),
-                Row(
-                  children: [
-                     Expanded(
-                     child: Text('Você Possui ${todos.length} Tarefas Pendentes',
-                       style: const TextStyle(
-                         fontWeight: FontWeight.w500,
-                         fontSize: 14,
-                       ),
+                const SizedBox(
+                  height: 16,
+                ),
 
-                     ),
-                   ),
-                    const SizedBox(width: 8,),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.cyan,
-                        onSurface: Colors.green,
-                        elevation: 20,
-                        shadowColor: Colors.black12,
-                        padding: const EdgeInsets.all(15),
-                      ),
-                      child: const Text('Limpar Tudo'
-
-                      ),
-                      onPressed: () {},
-                    ),
-                  ],
-                )
               ],
             ),
           ),
         ),
+        bottomNavigationBar: BottomAppBar(
 
+          child: Row(
+            children: [
+              const SizedBox(
+                width: 5,
+              ),
+              Expanded(
+                child: Text(
+                  'Você Possui ${todos.length} Tarefas Pendentes',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 14,
+
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.cyan,
+                  onSurface: Colors.green,
+                  elevation: 20,
+                  shadowColor: Colors.black12,
+                  padding: const EdgeInsets.all(15),
+
+                ),
+                child: const Text('Limpar Tudo'),
+                onPressed: () {
+                  dialogConfimation();
+                },
+              ),
+              const SizedBox(
+                width: 15,
+              )
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  void onDelete(Todo todo) {
+    todoDelete = todo;
+    todoPosDelete = todos.indexOf(todo);
+    setState(() {
+      todos.remove(todo);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(
+        'A Tarefa ${todo.title} Foi Removida Com Susseso',
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      backgroundColor: const Color(0xff00f0f0),
+      action: SnackBarAction(
+        label: 'Desfazer',
+        textColor: Colors.red,
+        onPressed: () {
+          setState(() {
+            todos.insert(todoPosDelete!, todoDelete!);
+          });
+        },
+      ),
+    ));
+  }
+
+  void dialogConfimation() {
+    showDialog(
+      context: context,
+      builder: (context) =>  AlertDialog(
+        title: const Text('Limpar Tudo?'),
+        content: const Text('tem certeza que deseja proseguir com esta ação?'),
+        actions: [
+          TextButton(onPressed: () {
+
+            Navigator.of(context).pop();
+            deletAllTodos();
+          }, child: Text('Sim')),
+          TextButton(onPressed: (){
+            Navigator.of(context).pop();
+          }, child: Text('Não')),
+        ],
+      ),
+    );
+  }
+
+  void deletAllTodos(){
+    setState(() {
+      todos.clear();
+    });
+
   }
 }
--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,47 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import 'package:todo_list/model/todo.dart';
 
 
 class TodoListItem extends StatelessWidget {
-  const TodoListItem({Key? key, required this.todo}) : super(key: key);
+  const TodoListItem({Key? key, required this.todo, required this.onDelete}) : super(key: key);
 
   final Todo todo;
+  final Function(Todo) onDelete;
 
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        color: Colors.grey[300],
-      ),
+    Future.delayed(Duration.zero, () async {
+      // Your Function
+    });
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Slidable(
 
-      padding: const EdgeInsets.all(10),
-      margin: const EdgeInsets.all(8) ,
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        //key: const ValueKey(0),
+
+
+        // The start action pane is the one at the left or the top side.
+        startActionPane: ActionPane(
+
+          extentRatio: 0.25,
+          // A motion is a widget used to control how the pane animates.
+          motion: ScrollMotion(),
+
+          // A pane can dismiss the Slidable.
+        //  dismissible: DismissiblePane(onDismissed: () {onDelete(todo);}),
+
+          // All actions are defined in the children parameter.
           children: [
-            Text(
-          DateFormat('dd/MM/yyyy').format(todo.dateTime),
-           style: const TextStyle(
-             fontSize: 10,
-           ),),
-           Text(  todo.title,
-              style: const TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: 18,
+            // A SlidableAction can have an icon and/or a label.
+            SlidableAction(
+              padding: const EdgeInsets.all(1),
 
-              ),
+              onPressed: ((context) {
+                onDelete(todo);
+              }),
+              backgroundColor: Color(0xFFFE4A49),
+              foregroundColor: Colors.white,
+              icon: Icons.delete,
+              label: 'Delete',
             ),
+
           ],
         ),
+
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Colors.grey[300],
+          ),
+
+                  padding: EdgeInsets.all(4),
+                  child: ListTile(
+                      title: Text(todo.title,
+                        style:const TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                              ) ,
+                           ),
+                      subtitle: Text(
+                              DateFormat('dd/MM/yyyy').format(todo.dateTime),
+                              style:const TextStyle(
+                                    fontSize: 12,
+                                 ),
+                              ),
+                    ),
+
+                  ),
       ),
-
-
     );
+
+
   }
+
 
 }
